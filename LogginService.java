abeec7493eb632e2fe659144344fd7d5f637d0a1
Wingemo <philip.wingemo@outlook.com>
2023-08-07 11:29:38 +0200

    Create LogginService.java

---
+++ LogginService.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.*;
+
+public class LogginService {
+
+    private static final Logger LOGGER = Logger.getLogger("SystemLogger");
+    private static FileHandler fileHandler;
+
+    public static Logger initialize(){
+        try {
+            PrintWriter writer = new PrintWriter("log.txt");
+            writer.print("");
+            writer.close();
+            fileHandler = new FileHandler("log.txt", true);
+        } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Exception occurred while initializing file handler", e);
+        }
+        LOGGER.addHandler(fileHandler);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+        LOGGER.setUseParentHandlers(false);
+        return LOGGER;
+    }
+
+    public static void log(String message) {
+        LOGGER.log(Level.INFO, message);
+    }
+
+}
